e5898327d99447e5a2c0d18b8c35a0b92a7d4739
Jordy Chamba <jordy_chamba@hotmail.com>
2026-02-20 12:46:15 -0500

    Cors

--- src/main/java/com/docker/backend/config/CorsConfig.java
+++ src/main/java/com/docker/backend/config/CorsConfig.java
@@ -15,7 +15,10 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost",
+                "http://localhost:80"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
